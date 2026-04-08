640dd1f7e419ca3a7e7b7f4dd7e82c7999b76d2b
Miguel Contreras <mcontrerasjaen@gmail.com>
2026-04-08 21:10:36 +0000

    correccion de ortografia

--- src/front/pages/Layout.jsx
+++ src/front/pages/Layout.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Outlet, useLocation } from "react-router-dom"; // Importamos useLocation
+import { Outlet, useLocation } from "react-router-dom";
 import ScrollToTop from "../components/ScrollToTop";
 import { Sidebar } from "../components/Sidebar";
 import { Footer } from "../components/Footer";
